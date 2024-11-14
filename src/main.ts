bb4a6613bb7363b66890c607e8f249f6d0d833a6
tufusa <me@tufusa.net>
2024-11-14 23:16:11 +0900

    chore: 実行をCloudflare Workerに移行 (#18)
    * chore: .dev.varsをignore
    
    * chore: wranglerのセットアップ
    
    * chore: Workerで実行できるよう変更
    
    * chore(biome): format

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import cron from "node-cron";
 import { update } from "./feature/bot";
 import { log } from "./feature/log";
 
 log("Marilami is up");
 
-cron.schedule("0 0 0 * * *", update);
+export interface Env {
+  TOKEN: string;
+  GUILD_ID: string;
+  CHANNEL_ID: string;
+  NAME: string;
+  ACTION: string;
+  DOOMSDAY: string;
+}
+
+const handler: ExportedHandler<Env> = {
+  fetch: async () => {
+    return new Response("Marilami is up.");
+  },
+  scheduled: (_controller, env, ctx) => {
+    return ctx.waitUntil(update(env));
+  },
+};
+
+export default handler;
